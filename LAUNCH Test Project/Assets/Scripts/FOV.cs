6e6dbfb15eef2faefa8f4cbfd37e14461e3fb6f7
Nick Hanoski <nickhanoski34@gmail.com>
2021-03-03 21:55:46 -0600

    Added Change FOV Slider to UI
    Added Change FOV Slider to UI

---
+++ LAUNCH Test Project/Assets/Scripts/FOV.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CameraToolkit
+{
+	public class FOV : MonoBehaviour
+	{
+		[SerializeField]
+		private Slider mainSlider;
+		[SerializeField]
+		private int speed;
+
+        public void Start()
+        {
+			mainSlider.value = Camera.main.fieldOfView / 179;
+
+		}
+
+        public void SliderMoved()
+		{
+			
+			Camera.main.fieldOfView = Mathf.MoveTowards(Camera.main.fieldOfView, mainSlider.value*179, Time.deltaTime * speed);
+
+			Debug.Log(mainSlider.value * 179);
+		}
+	}
+}
